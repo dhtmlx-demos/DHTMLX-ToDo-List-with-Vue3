907ef5fbccbeceb275eb0d5055c931b9128ed276
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:22:11 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ToDo from "./components/ToDo.vue";
+import { getData } from "./data";
+
+export default {
+  components: { ToDo },
+  data() {
+    const { users, projects, tasks } = getData();
+    return {
+      users,
+      projects,
+      tasks,
+    };
+  },
+};
+</script>
+
+<template>
+  <ToDo :users="users" :tasks="tasks" :projects="projects" />
+</template>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,23 @@
+<script>
+import { ToDo } from "@dhx/trial-todolist";
+import "@dhx/trial-todolist/dist/todo.css";
+
+export default {
+  props: ["tasks", "users", "projects"],
+
+  mounted() {
+    new ToDo(this.$refs.cont, {
+      users: this.users,
+      tasks: this.tasks,
+      projects: this.projects,
+    });
+  },
+  unmounted() {
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div ref="cont" style="width: 100%; height: 100%"></div>
+</template>
